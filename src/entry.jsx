6a6db71635ba607491ec59210cd6ddee08184499
Frank Dellaert <dellaert@gmail.com>
2020-08-04 09:30:07 -0400

    cloned gh-pages-jekyll-react template here

---
+++ src/entry.jsx
@@ -0,0 +1,9 @@
+// @flow
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+
+ReactDOM.render(<Component1 />, document.getElementById('component_1'));
+ReactDOM.render(<Component2 />, document.getElementById('component_2'));
